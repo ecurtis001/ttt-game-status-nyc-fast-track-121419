741009dcc057b3cd1b18ac270cee4833d20da9d9
Elijah Curtis <ejcurtis001@gmail.com>
2019-12-22 19:49:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,8 @@ end
  
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return [win_index_1,win_index_2,win_index_3] # return the win_combination indexes that won.
-  elsif
+  elsif  position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return [win_index_1,win_index_2,win_index_3] # return the win_combination indexes that won.
   else
     false
   end
