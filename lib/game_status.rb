e4bf420c7e24be89db20f52be5bff1774c77e964
Elijah Curtis <ejcurtis001@gmail.com>
2019-12-22 19:51:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,17 +15,13 @@ WIN_COMBINATIONS = [[ 0,1,2], [3,4,5],[6,7,8],[0,3,6], [1,4,7],[2,5,8],[0,4,8],[
        position_1 = board[win_index_1] # load the value of the board at win_index_1
   position_2 = board[win_index_2] # load the value of the board at win_index_2
   position_3 = board[win_index_3] # load the value of the board at win_index_3
-end
  
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return [win_index_1,win_index_2,win_index_3] # return the win_combination indexes that won.
-  elsif  position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return [win_index_1,win_index_2,win_index_3] # return the win_combination indexes that won.
+    return win_combination
   else
     false
   end
 end
-  
-
+end
  
  
